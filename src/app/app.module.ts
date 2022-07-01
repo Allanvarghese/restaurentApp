a00f8ac08ebdca4ba40a85d9cb6f10a1b594a73e
Allanvarghese <allanvarghese12@gmail.com>
2022-07-01 13:40:19 +0530

    Dash installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurentDashComponent
   ],
   imports: [
     BrowserModule
