7ad06031632bac5baf489c006e4d84ddbb2da6bf
Allanvarghese <allanvarghese12@gmail.com>
2022-07-02 14:42:09 +0530

    Added HttpClientModule in app.module.ts for comunicating HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from "@angular/common/http"
 
 
 import { AppComponent } from './app.component';
@@ -16,6 +16,7 @@ import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule
 
   ],
   providers: [],
