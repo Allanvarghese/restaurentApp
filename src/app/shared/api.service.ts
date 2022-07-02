992b2ebf67df2dc83f3f54bcc2dc4cecea6e8662
Allanvarghese <allanvarghese12@gmail.com>
2022-07-02 16:22:36 +0530

    REST in  Api Service

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+  // here i will define POST,GET,PUT,DELETE
+
+  //CREATE restaurent using POST method
+  postRestaurent(data:any){
+    return this._http.post<any>("https://jsonplaceholder.typicode.com/posts",data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  //Get restaurent data using GET Method
+  getRestaurent(){
+    return this._http.get<any>("https://jsonplaceholder.typicode.com/posts").pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  //Update Restaurent data using PUT method
+  updateRestaurent(data:any,id:number){
+    return this._http.put<any>("https://jsonplaceholder.typicode.com/posts/"+id,data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+    //delete Restaurent data using DELETE method
+    deleteRestaurent(id:number){
+      return this._http.delete<any>("https://jsonplaceholder.typicode.com/posts/"+id).pipe(map((res:any)=>{
+        return res;
+      }))
+    }
 }
