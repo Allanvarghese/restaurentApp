b127b1927eaa34dae87ea6d3c721d92fbc4902f0
Allanvarghese <allanvarghese12@gmail.com>
2022-07-02 13:28:04 +0530

    created a modal and created a form from getbootstrap.com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+
 
 import { AppComponent } from './app.component';
 import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.component';
@@ -8,9 +11,12 @@ import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.compo
   declarations: [
     AppComponent,
     RestaurentDashComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/restaurent-dash/restaurent-dash.component.ts
+++ src/app/restaurent-dash/restaurent-dash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurent-dash',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurentDashComponent implements OnInit {
 
-  constructor() { }
+  formValue!: FormGroup
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      services: ['']
+
+    })
   }
 
 }
