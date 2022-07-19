bfb8e33514639c75b76107d8f09810a758076a4f
Allanvarghese <allanvarghese12@gmail.com>
2022-07-19 11:11:07 +0530

    complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginform!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private _http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginform = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  login() {
+    this._http.get<any>("http://jsonplaceholder.typicode.com/signup").subscribe(res => {
+      const user = res.find((a: any) => {
+        return a.email === this.loginform.value.email && a.password === this.loginform.value.password
+      })
+      if (user) {
+        alert("login successfully");
+        this.loginform.reset();
+        this.router.navigate(['restaurent']);
+      }
+      else {
+        alert("user not found!!!!")
+      }
+    },
+      err => {
+        alert("server error")
+      }
+    )
+
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder ,FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signupform!:FormGroup
+  constructor(private formBuilder:FormBuilder , private _http:HttpClient ,private router: Router) { }
 
   ngOnInit(): void {
+    this.signupform= this.formBuilder.group({
+      name:[''],
+      email:[''],
+      mobile:[''],
+      password:[''],
+
+    })
+  }
+  // make method to create User
+  signUp(){
+    this._http.post<any>("http://jsonplaceholder.typicode.com/signup",this.signupform.value).subscribe(res=>{
+      alert("Signup successfully")
+      console.log(res);
+      this.signupform.reset();
+      this.router.navigate(['login'])
+    },
+    err=>{
+      alert("Error in Signup")
+    })
   }
 
 }
